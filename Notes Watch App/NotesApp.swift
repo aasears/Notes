28f05fbbe4dd25f307da091881b4f804b115ea68
Aaron Sears <aeroaaron200@gmail.com>
2022-09-05 17:31:33 -0600

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Aaron Sears on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
